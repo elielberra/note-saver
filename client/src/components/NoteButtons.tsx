8f26b08e9bf630e13fc81669e2ca103b1f1534f2
Eliel <berraeliel@gmail.com>
2024-03-29 14:43:54 -0300

    Improved  note buttons

--- client/src/components/NoteButtons.tsx
+++ client/src/components/NoteButtons.tsx
@@ -4,32 +4,38 @@ import ArchivedIcon from "./icons/ArchivedIcon";
 import DeleteIcon from "./icons/DeleteIcon";
 import "./NoteButtons.css";
 import { NoteProps } from "./Note";
+import AddIcon from "./icons/AddIcon";
 
 export default function NoteButtons({ tags }: { tags: NoteProps["tags"] }) {
   return (
     <div className="note-btns">
+      <div id="del-arch-btns">
+        <Button
+          id="delete-btn"
+          className="note-btn del-arch-btn"
+          Icon={DeleteIcon}
+          iconProps={{ height: 17, fill: "white" }}
+        />
+        <Button
+          id="archive-btn"
+          className="note-btn del-arch-btn"
+          Icon={ArchivedIcon}
+          iconProps={{ height: 17 }}
+        />
+      </div>
       {tags.map((tag) => {
         return (
-          <Button
-            className="note-btn tag-btn"
-            text={tag}
-            Icon={CrossIcon}
-            iconProps={{ fill: "white", height: 17 }}
-          />
+          <div className="note-btn tag">
+            <span className="tag-text">{tag}</span>
+            <Button
+              className="delete-tag-icon"
+              Icon={CrossIcon}
+              iconProps={{ fill: "white", height: 19 }}
+            />
+          </div>
         );
       })}
-      <Button
-        id="archive-btn"
-        className="note-btn del-arch-btn"
-        Icon={ArchivedIcon}
-        iconProps={{ height: 17 }}
-      />
-      <Button
-        id="delete-btn"
-        className="note-btn del-arch-btn"
-        Icon={DeleteIcon}
-        iconProps={{ height: 17, fill: "white" }}
-      />
+      <Button id="add-tag-btn" className="note-btn" Icon={AddIcon} iconProps={{ height: 22 }} />
     </div>
   );
 }
