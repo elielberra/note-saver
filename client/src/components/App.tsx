a2e96e537c36ec530712e3f5fd4aab3aa6c56445
Eliel <berraeliel@gmail.com>
2024-04-06 22:06:16 -0300

    Fetch notes from DB

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,20 +1,8 @@
 import Header from "./Header";
 import NoteActions from "./NoteActions";
 import Notes from "./Notes";
-import { useEffect } from "react";
-import "./App.css"
 
 export default function App() {
-  useEffect(() => {
-    async function fetchData() {
-      const res = await fetch("/notes")
-      // const data = await res.json()
-      // console.log(data)
-    }
-    fetchData();
-  }, [])
-  
-  fetch("/test")
   return (
     <>
       <Header />
--- client/src/components/Notes.tsx
+++ client/src/components/Notes.tsx
@@ -1,16 +1,24 @@
+import { useState, useEffect } from "react";
 import Note from "./Note";
 import "./Notes.css";
-import {NoteT} from "@backend/types"
+import { NoteT } from "@backend/types";
 
-const notes: NoteT[] = [
-  {
-    content: "Hi! I am a note :)",
-    tags: ["Tag", "Interna"],
-    active: true
-  }
-];
 export default function Notes() {
-  return <div id="notes-section">
-    {notes.map((note, index) => <Note key={index} content={note.content} tags={note.tags}/>)}
-  </div>;
+  const [notes, setNotes] = useState<NoteT[]>([]);
+  useEffect(() => {
+    async function fetchNotes() {
+      const response = await fetch("/notes");
+      const notes: NoteT[] = await response.json();
+      setNotes(notes)
+    }
+    fetchNotes();
+  }, []);
+
+  return (
+    <div id="notes-section">
+      {notes.map((note, index) => (
+        <Note key={index} content={note.content} tags={note.tags} />
+      ))}
+    </div>
+  );
 }
