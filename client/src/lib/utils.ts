44f7db87aed70fe5c85de7892060184a92146902
eli <berraeliel@gmail.com>
2024-06-25 17:44:02 -0300

    Hide error output on production

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -7,14 +7,17 @@ export async function fetchNotes(
 ) {
   try {
     const response = await fetch(`/notes?areActive=${notesStatus}${filteringText ? `&filteringText=${filteringText}` : ''}`);
-  if (!response.ok) {
-    throw new Error(`Error while fetching the notes. Request Status Code: ${response.status}`);
-  }
-  const activeNotes: NoteT[] = await response.json();
-  setNotes(activeNotes);
+    if (!response.ok) {
+      throw new Error(`Error while fetching the notes. Request Status Code: ${response.status}`);
+    }
+    const activeNotes: NoteT[] = await response.json();
+    setNotes(activeNotes);
   } catch (error) {
-    // TODO: Hide error output on production
-    console.error("Error while fetching the notes:", error);
+    console.error("Error while fetching the notes");
+    if (!isProductionEnv()) console.error(error)
   }
-  
+}
+
+export function isProductionEnv() {
+  return process.env.NODE_ENV === 'production';
 }
