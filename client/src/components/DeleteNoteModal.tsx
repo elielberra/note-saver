dd608f32f4304e9047fa011ac2a005e9d4f97a8c
eli <berraeliel@gmail.com>
2024-07-02 23:05:41 -0300

    Added portal for modal

--- client/src/components/DeleteNoteModal.tsx
+++ client/src/components/DeleteNoteModal.tsx
@@ -1,6 +1,7 @@
 import Button from "./Button";
 import Modal from "./Modal";
 import "./DeleteNoteModal.css";
+import { createPortal } from "react-dom";
 
 type DeleteNoteModalProps = {
   isOpen: boolean;
@@ -13,13 +14,14 @@ export default function DeleteNoteModal({
   handleCloseDeleteNoteModal,
   deleteNote
 }: DeleteNoteModalProps) {
-  return (
+  return createPortal(
     <Modal isOpen={isOpen} onClose={handleCloseDeleteNoteModal}>
       <p>Are you sure that you want to delete this note?</p>
       <div id="yes-no-btns">
         <Button className="modal-btn" id="no-btn" onClick={handleCloseDeleteNoteModal} text="No" />
         <Button className="modal-btn" id="yes-btn" onClick={deleteNote} text="Yes" />
       </div>
-    </Modal>
+    </Modal>,
+    document.getElementById("root")!
   );
 }
