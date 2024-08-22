246163293bee22cf1bb8132536dbb1545b33b359
eli <berraeliel@gmail.com>
2024-08-22 00:46:38 -0300

    Updated db initialize script

--- db/init.sql
+++ db/init.sql
@@ -1,5 +1,11 @@
 \connect note_keeper;
 
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(30) NOT NULL UNIQUE,
+    password VARCHAR(60) NOT NULL
+);
+
 CREATE TABLE notes (
     id SERIAL PRIMARY KEY,
     content VARCHAR(2500) NOT NULL,
@@ -10,4 +16,11 @@ CREATE TABLE notes (
     id SERIAL PRIMARY KEY,
     tag VARCHAR(20) NOT NULL,
     note_id INTEGER NOT NULL REFERENCES notes (id) ON DELETE CASCADE
+);
+
+CREATE TABLE sessions (
+    sid VARCHAR NOT NULL,
+    sess JSON NOT NULL,
+    expire TIMESTAMP NOT NULL,
+    PRIMARY KEY (sid)
 );
