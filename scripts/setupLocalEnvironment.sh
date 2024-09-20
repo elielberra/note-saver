be1c667aa32b818d07e1ec2b450893624dae3c01
eli <berraeliel@gmail.com>
2024-09-20 18:54:33 -0300

    Corrected order of script execution

--- scripts/setupLocalEnvironment.sh
+++ scripts/setupLocalEnvironment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 scriptDir=$(realpath $(dirname $0))
+bash "${scriptDir}"/insertDummyPasswords.sh
 bash "${scriptDir}"/configureDevSSLCerts.sh
 bash "${scriptDir}"/configureHostsFile.sh
-bash "${scriptDir}"/insertDummyPasswords.sh
