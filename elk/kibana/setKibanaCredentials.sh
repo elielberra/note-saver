c028dd797a0d90f3967bbc5e5c2e1460e6712014
eli <berraeliel@gmail.com>
2025-03-24 20:55:05 -0300

    feat: script enhancement

--- elk/kibana/setKibanaCredentials.sh
+++ elk/kibana/setKibanaCredentials.sh
@@ -9,8 +9,9 @@ elif [ x"${KIBANA_PASSWORD}" == x ]; then
 fi;
 
 echo "Setting the password for the user kibana_system";
-
-if curl -s -o /dev/null -w "%{http_code}" -X POST -u "elastic:${ELASTIC_PASSWORD}" -H "Content-Type: application/json" http://elastic:9200/_security/user/kibana_system/_password -d "{\"password\":\"${KIBANA_PASSWORD}\"}" | grep -q "200"; then
+statusCode=$(curl -s -o /dev/null -w "%{http_code}" -X POST -u "elastic:${ELASTIC_PASSWORD}" -H "Content-Type: application/json" http://elastic:9200/_security/user/kibana_system/_password -d "{\"password\":\"${KIBANA_PASSWORD}\"}")
+echo "The status code of the request that attempted to set the credentials was: $statusCode"
+if [ "${statusCode}" -eq 200 ]; then
   echo "Password set successfully.";
   exit 0;
 else
