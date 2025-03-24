5d65ac260c2ad1aa212b7f90cbe55bb7a89cfd75
eli <berraeliel@gmail.com>
2025-03-24 14:03:16 -0300

    feat: updated script for setting up env and passwords

--- scripts/setupLocalEnvironment.sh
+++ scripts/setupLocalEnvironment.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
 scriptDir=$(realpath $(dirname $0))
-bash "${scriptDir}"/insertDummyPasswords.sh
-bash "${scriptDir}"/configureDevSSLCerts.sh
-bash "${scriptDir}"/configureHostsFile.sh
-
-# Add script for sudo sysctl -w vm.max_map_count=262144
+bash "${scriptDir}/insertDummyPasswords.sh"
+bash "${scriptDir}/configureDevSSLCerts.sh""
+bash "${scriptDir}/configureHostsFile.sh"
+bash "${scriptDir}/addExecPermissions.sh"
+bash "${scriptDir}/expandVirtualMemory.sh"
