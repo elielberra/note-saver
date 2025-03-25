960d94286a7867551ce92940b2978f9389521f23
eli <berraeliel@gmail.com>
2025-03-24 23:12:59 -0300

    fix: minor correction on scripts

--- scripts/setupLocalEnvironment.sh
+++ scripts/setupLocalEnvironment.sh
@@ -2,7 +2,7 @@
 
 scriptDir=$(realpath $(dirname $0))
 bash "${scriptDir}/insertDummyPasswords.sh"
-bash "${scriptDir}/configureDevSSLCerts.sh""
+bash "${scriptDir}/configureDevSSLCerts.sh"
 bash "${scriptDir}/configureHostsFile.sh"
 bash "${scriptDir}/addExecPermissions.sh"
 bash "${scriptDir}/expandVirtualMemory.sh"
