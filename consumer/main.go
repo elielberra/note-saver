2384ea63a2fb837a1ea6fa4f0e0d8bd0c9757d78
eli <berraeliel@gmail.com>
2025-03-29 02:43:48 -0300

    Moved elastic indexing logic to separate go file

--- consumer/main.go
+++ consumer/main.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
 	"log"
-	"os"
 
-	"github.com/elastic/go-elasticsearch/v8"
 	"github.com/joho/godotenv"
 	"github.com/rabbitmq/amqp091-go"
 )
@@ -22,33 +18,7 @@ func main() {
 
 	for msg := range messages {
 		go func(msg amqp091.Delivery) {
-			cfg := elasticsearch.Config{
-				Addresses: []string{
-					fmt.Sprintf("http://%s:9200", os.Getenv("ELASTIC_DOMAIN")),
-				},
-				Username: os.Getenv("ELASTIC_USER"),
-				Password: os.Getenv("ELASTIC_PASSWORD"),
-			}
-			es, err := elasticsearch.NewClient(cfg)
-			if err != nil {
-				log.Printf("Error creating the Elasticsearch client: %s", err)
-				return
-			}
-
-			res, err := es.Index(
-				os.Getenv("ELASTIC_INDEX"),
-				bytes.NewReader(msg.Body),
-			)
-			defer res.Body.Close()
-			if err != nil {
-				log.Printf("Error while trying to index a log: %s", err)
-				return
-			}
-			if res.IsError() {
-				log.Printf("Error on the Response to Elasticsearch - Status code: %s", res.Status())
-				return
-			}
-			fmt.Println("Log successfully sent to Elasticsearch") // TODO: Delete me
+			sendToElastic(msg)
 		}(msg)
 	}
 	log.Println("The RabbitMQ channel has been closed")
