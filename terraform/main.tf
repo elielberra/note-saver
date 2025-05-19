7b53fc3afc342842efa261cf4b85431b63c98d0c
eli <berraeliel@gmail.com>
2025-05-19 02:20:20 -0300

    Initial VPC an EKS implementation

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,31 +2,46 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.98.0"
     }
   }
 }
 
 provider "aws" {
-  region     = "us-east-1"
+  region = "us-east-1"
 }
 
 module "vpc" {
-  source      = "./modules/vpc"
-  vpc_name    = "notesaver-k8s-cluster-vpc"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.21.0"
+
+  name            = "notesaver-vpc"
+  cidr            = "10.0.0.0/16"
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
 }
 
-output "created_vpc_name" {
-  description = "The name of the VPC created by the 'vpc' module."
-  value       = module.vpc.vpc_name
-}
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.36.0"
 
-output "created_vpc_id" {
-  description = "The ID of the VPC created by the 'vpc' module."
-  value       = module.vpc.vpc_id
-}
+  cluster_name                             = "k8s-notesaver"
+  cluster_version                          = "1.32"
+  enable_cluster_creator_admin_permissions = true
+  cluster_endpoint_public_access           = true
+  vpc_id                                   = module.vpc.vpc_id
+  subnet_ids                               = module.vpc.private_subnets
 
-output "created_public_subnet_id" {
-  description = "The public subnet ID/s created by the 'vpc' module."
-  value       = module.vpc.subnet_id
+  eks_managed_node_groups = {
+    default = {
+      desired_size   = 1
+      max_size       = 2
+      min_size       = 1
+      instance_types = ["t3.medium"]
+    }
+  }
 }
