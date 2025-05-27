798484c720de539694d9e08c216c87a316ceecaf
eli <berraeliel@gmail.com>
2025-05-27 18:43:26 -0300

    feat: scrpit for creating postgresql secrets

---
+++ k8s/createPostgresqlSecrets.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Load environment variables from .env
+source "./db/files/.env"
+
+# Delete the secret if it exists
+kubectl delete secret postgresql-passwords --ignore-not-found
+
+# Create the secret using values from the .env file
+kubectl create secret generic postgresql-passwords \
+  --from-literal=password="$POSTGRES_USER" \
+  --from-literal=postgres-password="$POSTGRES_PASSWORD"
