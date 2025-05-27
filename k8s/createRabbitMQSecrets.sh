684b1ae196ec0932b854efdf22991396344586b4
eli <berraeliel@gmail.com>
2025-05-27 18:43:35 -0300

    fix: corrected file paths

--- k8s/createRabbitMQSecrets.sh
+++ k8s/createRabbitMQSecrets.sh
@@ -2,10 +2,10 @@
 
 kubectl delete secret rabbitmq-conf --ignore-not-found
 kubectl create secret generic rabbitmq-conf \
-  --from-file=rabbitmq.conf=../rabbitmq/rabbitmq-k8s.conf
+  --from-file=rabbitmq.conf=rabbitmq/files/rabbitmq.conf
 
 kubectl delete secret rabbitmq-definitions --ignore-not-found
 kubectl create secret generic rabbitmq-definitions \
-  --from-file="load_definition.json"=../rabbitmq/definitions.json
+  --from-file="load_definition.json"=rabbitmq/files/definitions.json
 
 # TODO: Change path of files
