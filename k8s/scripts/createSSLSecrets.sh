b4ac4994ab183d5988c53732281d6f27a666f00f
Eli <berraeliel@gmail.com>
2025-06-08 16:01:30 +0000

    fix: corrected paths on ssl secret creation

--- k8s/scripts/createSSLSecrets.sh
+++ k8s/scripts/createSSLSecrets.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# Directory paths
+scriptDir=$(dirname $0)
+k8sProjectDir=$(realpath "${scriptDir}/..")
+cd "${k8sProjectDir}"
+
 kubectl delete secret notesaver-tls --ignore-not-found
 kubectl create secret tls notesaver-tls \
-  --cert=../ssl-certs/cert.pem \
-  --key=../ssl-certs/cert-key.pem
-# TODO: Evaluate certs path
+  --cert=./ssl-certs/cert.pem \
+  --key=./ssl-certs/cert-key.pem
--- k8s/scripts/prepareMinikube.sh
+++ k8s/scripts/prepareMinikube.sh
@@ -11,3 +11,4 @@ flux install
 minikube addons enable ingress
 bash "${rootProjectDir}/scripts/configureDevSSLCerts.sh" "--environment" "${ENVIRONMENT}"
 bash "${rootProjectDir}/scripts/configureHostsFile.sh" "--environment" "${ENVIRONMENT}"
+bash "${rootProjectDir}/k8s/scripts/createSSLSecrets.sh"
