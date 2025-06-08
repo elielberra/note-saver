bae882067fc54c946dbc4ef4d9d0f3ae2de5e2e0
Eli <berraeliel@gmail.com>
2025-06-08 05:12:56 +0000

    fix: corrections on docker compose host file

--- scripts/configureHostsFile.sh
+++ scripts/configureHostsFile.sh
@@ -2,15 +2,16 @@
 
 # Entry for resolving domains to localhost IP
 pattern="127\.0\.0\.1\s*docker-compose\.notesaver docker-compose\.server\.notesaver"
-entry="127.0.0.1    notesaver server.notesaver"
+entry="127.0.0.1    docker-compose.notesaver docker-compose.server.notesaver"
 hostsFile="/etc/hosts"
+finalMessage="Completed configuring hosts file"
 
 # Check if the entry already exists in /etc/hosts
-if ! grep -E "${pattern}" "${hostsFile}"; then
-    # If it doesn't exist, append it to /etc/hosts
-    echo "${entry}" | sudo tee -a "${hostsFile}" > /dev/null
-    echo "Entry added to ${hostsFile}"
-else
+if grep -E "${pattern}" "${hostsFile}"; then
     echo "Entry already exists in ${hostsFile}"
+    echo "${finalMessage}"
 fi
-echo "Completed configuring hosts file"
+# If it doesn't exist, append it to /etc/hosts
+echo "${entry}" | sudo tee -a "${hostsFile}" > /dev/null
+echo "Entry added to ${hostsFile}"
+echo "${finalMessage}"
