fd8ae36faf96f52f01921249087724a7eb11ef5f
Eli <berraeliel@gmail.com>
2025-06-14 02:21:26 -0300

    chores: removed TODOs

--- k8s/scripts/installlNginxIngressController.sh
+++ k8s/scripts/installlNginxIngressController.sh
@@ -3,4 +3,4 @@
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.12.2/deploy/static/provider/cloud/deploy.yaml
 kubectl wait --namespace ingress-nginx   --for=condition=ready pod   --selector=app.kubernetes.io/component=controller   --timeout=120s
 
-# TODO: Use a declarative helmrelease on terraform to install the nginx controller
+# TODO: Use a declarative helmrelease on terraform to install the nginx controller and DELETE THIS SCRIPT
